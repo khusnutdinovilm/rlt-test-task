24e56da4ca9e0145affa8b8df14756bd656006f8
Ilmir Khusnutdinov <khusnutdinov.ilmir.w7@gmail.com>
2025-02-02 01:26:22 +0500

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueDevTools()],
+
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
+
+  base: process.env.NODE_ENV === 'production' ? '/rlt-test-task/' : '/',
+
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
